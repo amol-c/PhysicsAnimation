d9821ab8adbe01539f00a78a81f6530a960dc86d
amol-c <amolchaudhari@Amols-Mac-Pro.home>
2015-10-18 03:45:22 -0400

    Got the ViewController working with back button.

--- Example/PhysicsAnimation/ViewController.swift
+++ Example/PhysicsAnimation/ViewController.swift
@@ -7,17 +7,26 @@
 //
 
 import UIKit
+import PhysicsAnimation
 
 class ViewController: UIViewController {
+    var context: PhysicsControllerContext!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.context = PhysicsControllerContext(navigationController: self.navigationController!)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pushNextViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: .None)
+        let viewController = storyboard.instantiateViewControllerWithIdentifier("AnotherViewController")
+        self.navigationController?.pushViewController(viewController, animated: true)
     }
 
 }
